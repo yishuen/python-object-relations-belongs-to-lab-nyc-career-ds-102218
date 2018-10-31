58b95b1b83a790cab8fcd4f4f48e96076686cfb8
yishuen <limyishuen@gmail.com>
2018-10-31 09:16:56 -0400

    finished lab

--- car.py
+++ car.py
@@ -1,3 +1,5 @@
+from person import Person
+
 class Car:
 
     _all = []
@@ -8,3 +10,45 @@ class Car:
         self._year = year
         self._owner = owner
         Car._all.append(self)
+
+    @property
+    def make(self):
+        return self._make
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def year(self):
+        return self._year
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @classmethod
+    def cars_driven_by(self, occ):
+        chosen_occ = list(filter(lambda p: p._occupation == occ, Person._all))
+        chosen_cars = list(filter(lambda car: car.owner in chosen_occ, Car._all))
+        people = list(map(lambda p: p.owner.name, chosen_cars))
+        return people
+
+
+pam = Person("Pam Beasley", "Secretary")
+toyota = Car("Toyota", "Yaris", 2007, pam)
+
+michael = Person("Michael Scott", "Regional Manager")
+sebring = Car("Chrysler", "Sebring Convertible", 2004, michael)
+
+dwight = Person("Dwight Schrute", "Paper Salesperson")
+datsun = Car("Datsun", "280Z", 1978, dwight)
+
+meredith = Person("Meredith Palmer", "Purchaser - Supplier Relations")
+ford_minivan = Car("Ford", "Aerostar Minivan", 1997, meredith)
+
+jim = Person("Jim Halpert", "Paper Salesperson")
+corolla = Car("Toyota", "Corolla", 2000, jim)
+
+stanley = Person("Stanley Hudson", "Paper Salesperson")
+camry = Car ("Toyota", "Camry", 2008, stanley)
--- person.py
+++ person.py
@@ -1,8 +1,47 @@
 # import car class here
-from car import Car
+# from car import Car
 
 class Person:
+
+    _all = []
 
     def __init__(self, name, occupation):
         self._name = name
         self._occupation = occupation
+        Person._all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def occupation(self):
+        return self._occupation
+
+    @classmethod
+    def has_oldest_car(cls):
+        cars = Car._all
+        oldest = cars[0]
+        for car in cars:
+            if car.year < oldest.year:
+                oldest = car
+        return oldest.owner.name
+
+    @classmethod
+    def drives_a(cls, make):
+        car_makes = {}
+        for car in Car._all:
+            car_makes[car._owner] = car._make
+        matches = {}
+        for k, v in car_makes.items():
+            if v == make:
+                matches[k] = v
+        return list(map(lambda obj: obj.name, matches.keys()))
+
+    def drives_same_make_as_me(self):
+        my_make = list(filter(lambda car: car.owner == self, Car._all))[0]._make
+        print(my_make)
+        matches = list(filter(lambda car: car.make == my_make, Car._all))
+        matches_xself = list(filter(lambda car: car.owner != self, matches))
+        people = list(map(lambda car: car.owner.name, matches_xself))
+        return people
